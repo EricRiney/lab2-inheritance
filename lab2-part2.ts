bdce69f5580d22f396324e027d3da2dd76bc7254
EricRiney <fails.to.install@gmail.com>
2017-01-11 10:48:46 -0800

    blah

---
+++ lab2-part2.ts
@@ -0,0 +1,11 @@
+class partOne {
+    initialBalance: Number
+    ownerName: String;
+    constructor() {
+        this.initialBalance = 23;
+        this.ownerName = "name"
+    }
+    partOne() {
+        return "Hello, " + this.ownerName + "your inital balence is " + this.initialBalance;
+    }
+}
